b8c79297f677be49e3515054170e94aa4e16d1f4
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-04-01 13:25:07 +0500

    Setup admin layout, sidebar, header and admin routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,11 @@ import { Route, Routes } from "react-router-dom";
 import AuthLayout from "./components/auth/layout";
 import AuthLogin from "./pages/auth/login";
 import AuthRegister from "./pages/auth/register";
+import AdminLayout from "./components/admin-view/layout";
+import AdminDashboard from "./pages/admin-view/dashboard";
+import AdminFeatures from "./pages/admin-view/features";
+import AdminOrders from "./pages/admin-view/orders";
+import AdminProducts from "./pages/admin-view/products";
 
 const App = () => {
   return (
@@ -14,6 +19,13 @@ const App = () => {
         <Route path="/auth" element={<AuthLayout />}>
           <Route path="login" element={<AuthLogin />} />
           <Route path="register" element={<AuthRegister />} />
+        </Route>
+
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="products" element={<AdminProducts />} />
+          <Route path="orders" element={<AdminOrders />} />
+          <Route path="features" element={<AdminFeatures />} />
         </Route>
       </Routes>
     </div>
