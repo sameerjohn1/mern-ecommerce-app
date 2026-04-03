b483587529d493edf67f23f0b42af6ae8b6b80b5
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-04-03 09:46:57 +0500

    Added auth register page and reusable CommonForm component

--- client/src/pages/auth/register.jsx
+++ client/src/pages/auth/register.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import CommonForm from "@/components/common/form";
+import { registerFormControls } from "@/config";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const initialState = {
+  userName: "",
+  email: "",
+  password: "",
+};
 
 const AuthRegister = () => {
-  return <div>AuthRegister</div>;
+  const [formData, setFormData] = useState(initialState);
+
+  function onSubmit(e) {}
+
+  return (
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          Create new account
+        </h1>
+        <p className="mt-2">
+          Already have an account
+          <Link
+            className="font-medium ml-2 text-primary hover:underline"
+            to={"/auth/login"}
+          >
+            Login
+          </Link>
+        </p>
+      </div>
+
+      <CommonForm
+        formControls={registerFormControls}
+        buttonText={"Sign Up"}
+        formData={formData}
+        setFormData={setFormData}
+        onSubmit={onSubmit}
+      />
+    </div>
+  );
 };
 
 export default AuthRegister;
