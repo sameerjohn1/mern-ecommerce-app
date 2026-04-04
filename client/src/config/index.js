647e399d7821e8796a81314efe457acfd728dec9
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-04-04 09:50:35 +0500

    feat(auth): implement login page using reusable common form component

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -21,3 +21,20 @@ export const registerFormControls = [
     type: "password",
   },
 ];
+
+export const loginFormControls = [
+  {
+    name: "email",
+    label: "Email",
+    placeholder: "Enter your user email",
+    componentType: "input",
+    type: "email",
+  },
+  {
+    name: "password",
+    label: "Password",
+    placeholder: "Enter your user password",
+    componentType: "input",
+    type: "password",
+  },
+];
