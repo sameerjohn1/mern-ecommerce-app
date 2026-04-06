3557d680b53e8a50de9e7a536070e6050848a091
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-04-06 16:30:19 +0500

    feat(auth): implement auth check with protected routes and Redux state handling

--- server/routes/auth/auth-routes.js
+++ server/routes/auth/auth-routes.js
@@ -3,6 +3,7 @@ const {
   registerUser,
   loginUser,
   logoutUser,
+  authMiddleware,
 } = require("../../controllers/auth/auth-controller");
 
 const router = express.Router();
@@ -10,5 +11,10 @@ const router = express.Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
+
+router.get("/check-auth", authMiddleware, (req, res) => {
+  const user = req.user;
+  res.status(200).json({ success: true, message: "Authenticated User!", user });
+});
 
 module.exports = router;
