3904702e63f7eaa0f9618d4073b060471a3fd967
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-04-15 14:08:56 +0500

    feat(shop): add product slice with async thunk for fetching filtered products

---
+++ client/src/store/shop/product-slice/index.js
@@ -0,0 +1,40 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  isLoading: false,
+  productList: [],
+};
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+export const fetchAllFilteredProducts = createAsyncThunk(
+  "/products/fetchAllProducts",
+  async () => {
+    const result = await axios.get(`${API_URL}/api/shop/products/get`);
+
+    return result?.data;
+  },
+);
+
+const shopProductSlice = createSlice({
+  name: "shopingProducts",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllFilteredProducts.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchAllFilteredProducts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.productList = action.payload.data;
+      })
+      .addCase(fetchAllFilteredProducts.rejected, (state) => {
+        state.isLoading = false;
+        state.productList = [];
+      });
+  },
+});
+
+export default shopProductSlice.reducer;
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
 import adminProductSlice from "./admin/products-slice";
+import shopProductSlice from "./shop/product-slice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     adminProducts: adminProductSlice,
+    shopProducts: shopProductSlice,
   },
 });
 
