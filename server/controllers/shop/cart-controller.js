8404b7335af5babc296ccc40396e337056a52c0b
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-04-18 11:21:09 +0500

    feat(cart): add update cart item quantity endpoint with validation, error handling, and product data population

--- server/controllers/shop/cart-controller.js
+++ server/controllers/shop/cart-controller.js
@@ -108,7 +108,71 @@ const fetchCartItems = async (req, res) => {
   }
 };
 
+const updateCartItemQty = async (req, res) => {
+  try {
+    const { userId, productId, quantity } = req.body;
+
+    if (!userId || !productId || quantity <= 0) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid data provided!",
+      });
+    }
+
+    const cart = await Cart.findOne({ userId });
+    if (!cart) {
+      return res.status(404).json({
+        success: false,
+        message: "Cart not found!",
+      });
+    }
+
+    const findCurrentProductIndex = cart.items.findIndex(
+      (item) => item.productId.toString() === productId,
+    );
+
+    if (findCurrentProductIndex === -1) {
+      return res.status(404).json({
+        success: false,
+        message: "Cart item not present !",
+      });
+    }
+
+    cart.items[findCurrentProductIndex].quantity = quantity;
+    await cart.save();
+
+    await cart.populate({
+      path: "items.productId",
+      select: "image title price salePrice",
+    });
+
+    const populateCartItems = cart.items.map((item) => ({
+      productId: item.productId ? item.productId._id : null,
+      image: item.productId ? item.productId.image : null,
+      title: item.productId ? item.productId.title : "Product not found",
+      price: item.productId ? item.productId.price : null,
+      salePrice: item.productId ? item.productId.salePrice : null,
+      quantity: item.quantity,
+    }));
+
+    res.status(200).json({
+      success: true,
+      data: {
+        ...cart._doc,
+        items: populateCartItems,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Error",
+    });
+  }
+};
+
 module.exports = {
   addToCart,
   fetchCartItems,
+  updateCartItemQty,
 };
--- server/routes/shop/cart-routes.js
+++ server/routes/shop/cart-routes.js
@@ -3,11 +3,13 @@ const express = require("express");
 const {
   addToCart,
   fetchCartItems,
+  updateCartItemQty,
 } = require("../../controllers/shop/cart-controller");
 
 const router = express.Router();
 
 router.post("/add", addToCart);
 router.get("/get/:userId", fetchCartItems);
+router.put("/update-cart", updateCartItemQty);
 
 module.exports = router;
